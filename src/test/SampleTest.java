98494aceaffe40fa34340324c4709369581d07fa
Sakugawa Masataka <e185702@ie.u-ryukyu.ac.jp>
2018-11-06 09:11:23 +0900

    First commit

---
+++ src/test/SampleTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+    public int add(int a,int b){
+        int result = Sample.add(a:2, b:3);
+        assertEquals(expected:5,)
+    }
+
+
+}
